2f0e2870acc2c4f28f01064834fe6fc0d192ce8e
evanrepos <ivanagf04@gmail.com>
2026-06-26 18:23:29 -0300

    Prueba de reestructuracion de solución.

--- BBDD/00_Deploy_Completo.sql
+++ BBDD/00_Deploy_Completo.sql
@@ -7,24 +7,22 @@ Para realizar el despliegue completo, realizar los siguientes pasos:
 Desactivar cuando todo haya terminado.
 */
 
-:setvar ROOT "E:\evanrepos\Parques-Nacionales\BBDD"
+:r .\DDL\00_Borrado_BBDD.sql
+:r .\DDL\01_Creacion_BBDD.sql
+:r .\DDL\02_Creacion_Esquemas.sql
+:r .\DDL\03_Creacion_Tablas.sql
+:r .\DDL\04_Restricciones.sql
 
-:r "$(ROOT)\DDL\00_Borrado_BBDD.sql"
-:r "$(ROOT)\DDL\01_Creacion_BBDD.sql"
-:r "$(ROOT)\DDL\02_Creacion_Esquemas.sql"
-:r "$(ROOT)\DDL\03_Creacion_Tablas.sql"
-:r "$(ROOT)\DDL\04_Restricciones.sql"
+:r .\OPERACIONES\Administracion\00_Operaciones_Administracion.sql
+:r .\OPERACIONES\Administracion\02_Importacion_Administracion.sql
 
-:r "$(ROOT)\OPERACIONES\Administracion\00_Operaciones_Administracion.sql"
-:r "$(ROOT)\OPERACIONES\Administracion\02_Importacion_Administracion.sql"
+:r .\OPERACIONES\Comercial\00_Operaciones_Concesion.sql
+:r .\OPERACIONES\Comercial\00_Operaciones_Empresa.sql
+:r .\OPERACIONES\Comercial\02_Importacion_Comercial.sql
 
-:r "$(ROOT)\OPERACIONES\Comercial\00_Operaciones_Concesion.sql"
-:r "$(ROOT)\OPERACIONES\Comercial\00_Operaciones_Empresa.sql"
-:r "$(ROOT)\OPERACIONES\Comercial\02_Importacion_Comercial.sql"
+:r .\OPERACIONES\RRHH\00_Operaciones_Guias.sql
+:r .\OPERACIONES\RRHH\00_Operaciones_Guardaparques.sql
+:r .\OPERACIONES\RRHH\02_Importacion_RRHH.sql
 
-:r "$(ROOT)\OPERACIONES\RRHH\00_Operaciones_Guias.sql"
-:r "$(ROOT)\OPERACIONES\RRHH\00_Operaciones_Guardaparques.sql"
-:r "$(ROOT)\OPERACIONES\RRHH\02_Importacion_RRHH.sql"
-
-:r "$(ROOT)\OPERACIONES\Ventas\00_Operaciones_Ventas.sql"
-:r "$(ROOT)\OPERACIONES\Ventas\02_Importacion_Ventas.sql"
+:r .\OPERACIONES\Ventas\00_Operaciones_Ventas.sql
+:r .\OPERACIONES\Ventas\02_Importacion_Ventas.sql
